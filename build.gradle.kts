938b3a1c819bb049f775fa51a9cd65bfe7a26feb
たけのこII <yu2.aoki0229.@gmail.com>
2026-02-21 01:18:35 +0900

    a

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,7 @@
 plugins {
     kotlin("jvm") version "2.3.0"
+    `maven-publish`
+    id("cl.franciscosolis.sonatype-central-upload") version "1.0.2"
 }
 
 group = "io.github.takenoko4096"
@@ -16,4 +18,26 @@ dependencies {
 
 kotlin {
     jvmToolchain(21)
+}
+
+java {
+    withSourcesJar()
+    withJavadocJar()
+}
+
+publishing {
+    publications {
+        register<MavenPublication>("maven") {
+            pom {
+                name.set(project.name)
+                description.set("atode kaku yoon") // TODO
+                url.set("https://github.com/takenoko4096/DSLBrigadier")
+
+                licenses {
+                    name.set("MIT License")
+                    url.set("https://github.com/takenoko4096/DSLBrigadier/blob/master/LICENSE")
+                }
+            }
+        }
+    }
 }
