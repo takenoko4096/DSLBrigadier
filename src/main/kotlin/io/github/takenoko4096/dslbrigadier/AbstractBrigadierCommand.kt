3bc02ae273a16ef9f62fa9744d425f113cd0e97e
たけのこII <yu2.aoki0229.@gmail.com>
2026-03-12 19:15:28 +0900

    a

--- src/main/kotlin/io/github/takenoko4096/dslbrigadier/AbstractBrigadierCommand.kt
+++ src/main/kotlin/io/github/takenoko4096/dslbrigadier/AbstractBrigadierCommand.kt
@@ -1,14 +1,24 @@
 package io.github.takenoko4096.dslbrigadier
 
+import com.mojang.brigadier.builder.LiteralArgumentBuilder
 import io.github.takenoko4096.dslbrigadier.node.ConfigurableCommandNode
 import io.papermc.paper.command.brigadier.CommandSourceStack
 
 abstract class AbstractBrigadierCommand(name: String) : ConfigurableCommandNode<CommandSourceStack>(name) {
+    private var built: Boolean = false
+
     protected abstract fun command()
 
     internal fun create(): DSLCommand<CommandSourceStack> {
-        return createCommand(name) {
+        if (!built) {
             command()
         }
+
+        return DSLCommand(
+            this.argumentBuilder as LiteralArgumentBuilder<CommandSourceStack>,
+            name,
+            description,
+            aliases
+        )
     }
 }
